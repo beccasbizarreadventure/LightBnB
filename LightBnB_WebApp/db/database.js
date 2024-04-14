554ea5c8c06757122a67da016344250bc9a9b0ef
Rebecca <rebecca90469@gmail.com>
2024-04-14 18:28:55 -0400

    Add WHERE 1=1 to initial queryString

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -159,11 +159,12 @@ const queryParams = [];
   SELECT properties.*, avg(property_reviews.rating)
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
   `;
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `AND city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
@@ -190,12 +191,9 @@ const queryParams = [];
     queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
   }
 
-  queryString += `
-  ORDER BY cost_per_night
-  `;
-
   queryParams.push(limit);
   queryString += `
+  ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
