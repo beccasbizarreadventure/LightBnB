4c10a786fef7ba341518f42a679c36abec820214
Rebecca <rebecca90469@gmail.com>
2024-04-14 19:06:36 -0400

    Fix ratings to display ratings with properties

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -156,7 +156,7 @@ const getAllProperties = (options, limit = 10) => {
 const queryParams = [];
 
   let queryString = `
-  SELECT properties.*, avg(property_reviews.rating)
+  SELECT properties.*, AVG(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   WHERE 1=1
